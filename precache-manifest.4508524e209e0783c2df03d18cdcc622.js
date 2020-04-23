fe38f50c1f2013c223c05b373f2f0d01618849e6
Ridha Mezrigui <47607249+ridhamz@users.noreply.github.com>
2020-04-23 19:32:14 +0100

    Updates

---
+++ precache-manifest.4508524e209e0783c2df03d18cdcc622.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69aadc9f91b971faacfc10bae24ab39e",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "20b1e3a0d161228c175e",
+    "url": "/movies-app/static/css/2.8d7044f0.chunk.css"
+  },
+  {
+    "revision": "bd0b5ea862a19dfd8ef2",
+    "url": "/movies-app/static/css/main.1e766d89.chunk.css"
+  },
+  {
+    "revision": "20b1e3a0d161228c175e",
+    "url": "/movies-app/static/js/2.5a588b3b.chunk.js"
+  },
+  {
+    "revision": "c560b744118e776c5a9fe14e0546e12a",
+    "url": "/movies-app/static/js/2.5a588b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd0b5ea862a19dfd8ef2",
+    "url": "/movies-app/static/js/main.b7d9c3c8.chunk.js"
+  },
+  {
+    "revision": "d6d8252f4cfd7f1c3a9a",
+    "url": "/movies-app/static/js/runtime-main.89fddfa0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movies-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movies-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movies-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movies-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movies-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
